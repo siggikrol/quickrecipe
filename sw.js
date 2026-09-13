702971e7bb270912dd93c6087d243e25aadc1232
Sigurdur Bjarnason <sbjarnason@pbl.ca>
2026-09-13 15:22:42 +0200

    Add 162 translated cake recipes from Gotteri with section plans and PWA cache bump

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE = 'quickrecipe-v9';
+const CACHE = 'quickrecipe-v10';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
-  './style.css?v=9',
+  './style.css?v=10',
   './app.js',
-  './app.js?v=9',
+  './app.js?v=10',
   './recipes.json',
-  './recipes.json?v=9',
+  './recipes.json?v=10',
   './manifest.webmanifest',
 ];
 
